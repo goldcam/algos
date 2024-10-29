c0367726335b55606c800a8167dbb126bc942783
cameron.townsend <13910756+goldcam@users.noreply.github.com>
2024-10-28 21:05:25 -0700

    finish stacks

--- stacksAndGueues/stacks.js
+++ stacksAndGueues/stacks.js
@@ -73,3 +73,57 @@ function reverseString(str) {
 
 
 console.log(reverseString("abcde"))  // edcba 
+
+
+function findPrecedingSmallerElements(arr) {
+  let stack = [];
+  let result = new Array(arr.length).fill(-1);
+
+  for (let i = 0; i < arr.length; i++) {
+      while (stack.length > 0 && stack[stack.length - 1] >= arr[i]) {
+          stack.pop();
+      }
+      if (stack.length > 0) {
+          result[i] = stack[stack.length - 1];
+    
+      }
+      stack.push(arr[i]);
+  }
+  return result;
+}
+
+
+console.log(findPrecedingSmallerElements([1, 3, 2, 5, 4, 7]));
+// Output: [ -1, 1, 1, 2, 2, 4 ]
+console.log(findPrecedingSmallerElements([5, 4, 3, 2, 1]));
+// Output: [ -1, -1, -1, -1, -1 ]
+
+class MinStack {
+  constructor(){
+      this.stack = [];
+      this.minStack = [];
+  }
+
+  push(val) {
+      this.stack.push(val);
+      if (this.minStack.length === 0 || val <= this.getMin()) {
+          this.minStack.push(val);
+      }
+  }
+
+  pop() {
+       const item = this.stack.pop(); 
+       if (item === this.getMin()) {
+           this.minStack.pop();
+       }
+       return item;
+ }
+
+  top() {
+      return this.stack[this.stack.length - 1];
+  }
+
+  getMin() {
+      return this.minStack[this.minStack.length - 1];
+  }
+}
